4d2ee33cb50c6377665bbec0b1ea9fbb6a3cee06
Nikola Kasev <nikola.kasev@gmail.com>
2016-02-17 09:27:05 +0100

    extending the unit tests

--- src/test/scala/PredicateSpecification.scala
+++ src/test/scala/PredicateSpecification.scala
@@ -1,4 +1,4 @@
-import nl.bridgeworks.akka.rete.{ConceptWithValue, ConceptOnly, Simple, TerminalMain}
+import nl.bridgeworks.akka.rete._
 import org.scalatest.FlatSpec
 
 class PredicateSpecification extends FlatSpec {
@@ -6,5 +6,22 @@ class PredicateSpecification extends FlatSpec {
     assert(TerminalMain.predicate(Simple("Rocky Balboa"))(ConceptOnly("Rocky Balboa")))
     //TODO is AnyVal the right choice?
     assert(TerminalMain.predicate(Simple("Apollo Creed"))(ConceptWithValue("Apollo Creed", 98429)))
+    //checks the workings of an expression
+    assert(TerminalMain.predicate(ValueOp("temperature", GreaterThan, 100))(ConceptWithValue("temperature", 150)))
+    assert(TerminalMain.predicate(ValueOp("temperature", Equals, 100))(ConceptWithValue("temperature", 100)))
+    assert(TerminalMain.predicate(ValueOp("temperature", LessThan, 100))(ConceptWithValue("temperature", 90)))
+  }
+
+  it should "return false" in {
+    assert(TerminalMain.predicate(Simple("Philadelphia"))(ConceptOnly("New York")) == false)
+    //can't evaluate the expression if the concept has no associated value
+    assert(TerminalMain.predicate(ValueOp("temperature", GreaterThan, 100))(ConceptOnly("temperature")))
+    //checks the workings of an expression
+    assert(TerminalMain.predicate(ValueOp("speed", GreaterThan, 100))(ConceptWithValue("speed", 90)))
+    assert(TerminalMain.predicate(ValueOp("speed", Equals, 100))(ConceptWithValue("speed", 105)))
+    assert(TerminalMain.predicate(ValueOp("speed", LessThan, 100))(ConceptWithValue("speed", 120)))
+    //comparing two different concepts doesn't make sense
+    assert(TerminalMain.predicate(ValueOp("speed", GreaterThan, 100))(ConceptWithValue("pressure", 150)))
+    assert(TerminalMain.predicate(ValueOp("speed", GreaterThan, 100))(ConceptOnly("weight")))
   }
 }
